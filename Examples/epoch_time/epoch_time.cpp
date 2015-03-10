37386b0434115cd33d9f22838a2ad2c430763b76
Evan Hall <evan.tom.hall@gmail.com>
2015-03-10 15:47:06 -0500

    Epoch Time Example

---
+++ Examples/epoch_time/epoch_time.cpp
@@ -0,0 +1,59 @@
+/* Evan Hall 3/10/2015
+ *
+ * I NEED THESE COMPILATION FLAGS:
+ *
+ * TODO: add this flag during compilation
+ *			-lm
+ */
+
+#include <inttypes.h>	// needed for PRI%MAX
+						//					where PRIdMAX is the equivalent of d
+						//						(in "%d") for intmax_t values
+						//			  intmax_t
+						//					where intmax_t is an int type of maximum
+						//						supported width
+						//
+#include <math.h>		// needed for round(long double)
+						//			  1.0e6
+						//
+#include <stdio.h>		// needed for printf(...)
+						//
+#include <time.h>		// needed for time_t
+						//			  timespec
+						//			  clock_gettime(clockid_t, struct timespec *)
+						//			  CLOCK_REALTIME
+
+typedef struct time_s_ms {
+	time_t sec;
+	long msec;
+} time;
+
+int main() {
+
+	while (1) {
+	
+		struct time now = get_time_from_epoch();
+
+		//	*a printf flag defined outside the printf header
+		//	#change the decimal precision here (it's 3 here)
+		//
+		//					  /--  *  --/ /#/
+		printf("Current time: %"PRIdMAX".%03ld seconds since Epoch\n",
+			   (intmax_t)now.sec, now.msec);
+	}
+
+	return 0;
+}
+
+void time get_time_from_epoch()
+{
+	struct time result;
+	struct timespec spec;
+
+	clock_gettime(CLOCK_REALTIME, &spec);
+
+	result.sec = spec.tv_sec;
+	result.msec = round(spec.tv_nsec / 10e6);
+
+	return result;
+}
